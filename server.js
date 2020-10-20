987f32908ab55f117258ec95d5c9b4646c4d32b7
Mustafa Mustafa <mamustafa@gtcc.edu>
2020-10-20 09:30:46 -0400

    heroku fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.use(express.urlencoded({ extended: true }));
